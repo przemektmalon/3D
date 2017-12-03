98343aa449cd8dc4fbfacd6da3c3519a0ece2f40
Przemek Malon <przemekmalon@hotmail.co.uk>
2017-12-03 16:01:31 +0000

    Simple general profiler class

---
+++ 3D/Profiler.cpp
@@ -0,0 +1,21 @@
+#include "Profiler.hpp"
+#include "Engine.hpp"
+#include "QPC.hpp"
+#include <gl\glew.h>
+#include <gl\GL.h>
+
+void Profiler::start(std::string id)
+{
+	times[id].first.setMicroSeconds(Engine::qpc.now());
+}
+
+void Profiler::end(std::string id)
+{
+	times[id].second.setMicroSeconds(Engine::qpc.now() - times[id].first.getMicroSeconds());
+}
+
+void Profiler::glEnd(std::string id)
+{
+	glFinish();
+	times[id].second.setMicroSeconds(Engine::qpc.now() - times[id].first.getMicroSeconds());
+}
---
+++ 3D/Profiler.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+#include "Time.hpp"
+
+
+
+class Profiler
+{
+public:
+	Profiler() {}
+	~Profiler() {}
+
+	void start(std::string id);
+	void end(std::string id);
+	void glEnd(std::string id);
+
+	Time getTime(std::string id)
+	{
+		return times[id].second;
+	}
+
+private:
+
+	std::unordered_map<std::string, std::pair<Time,Time>> times;
+
+};
